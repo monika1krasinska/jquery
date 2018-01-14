abb07512da8c1dcda0c2fdb7bc9e9f34c8d2b845
Monika Krasińska <monika.krasinska@o2.pl>
2018-01-14 12:39:15 +0100

    commit -m "manipulacja-dom"

---
+++ js/3-manipulacja-dom.js
@@ -0,0 +1,23 @@
+'use strict';
+//$(function(){
+//    var textParagrafu = $('p#paragraf').text("Nowy paragraf o id paragraf");
+//    console.log($("p#paragraf").text());
+//})
+//var jakisTamParagraf =$("p.paragrafnext").html("<b>Hello</b>World")
+//var jakisTamParagraf = $("p.paragrafnext");
+//jakisTamParagraf.html("<b>Hello</b> World").after("tresc za elementem");
+//$(".paragrafnext").append("<br>Dodatkowy tekst paragrafu</br>");
+//$("h3").remove();
+//$("#second").empty();
+//$("h2").html("<span style='color:red';>Kolorowy span w h2</span>");
+//$("h1").css({"font-size":'90px', "color":"red", "background-color": "lime"}); 
+//var imie = $('input#imie').val();
+//console.log(imie);
+//$('input#imie').val('Damian');
+////pobiera atrybut
+//$("h1").attr("title") 
+////ustawia atrybut
+//$("h1").attr("title","Nagłówek h1");
+//Style dodane w js są ważniejsze niż stworzone w pliku!
+$('h1').addClass("blue");
+$(".paragrafnext").removeClass("paragrafnext");
