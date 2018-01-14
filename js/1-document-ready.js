abdf1b9b179093cc4706d14f6226ba046de20d24
Monika Krasińska <monika.krasinska@o2.pl>
2018-01-14 11:17:23 +0100

    commit -m "selektory"

---
+++ js/1-document-ready.js
@@ -0,0 +1,11 @@
+'use strict';
+//$(document).ready(function () {
+//    alert("Czolem, wczytałem DOM i jestem jQuery")
+//});
+$(function () {
+    //    $('#paragraf').css("color","red"); 
+    $('#paragraf').css({
+        "color": "red",
+        "background-color": "#000"
+    });
+})
---
+++ js/2-selektory.js
@@ -0,0 +1,9 @@
+'use strict';
+$(function(){
+    var paragrafy = $('p');
+    console.log(paragrafy);
+});
+var h2 = $('#second');
+console.log(h2);
+var paragraf = $('.paragraf');
+console.log(paragraf);
